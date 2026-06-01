3d04af6f9abca28f2e75f0d52d13c98cc66736ff
Baek Gisang <rltkd0101@gmail.com>
2026-06-01 15:04:45 +0900

    present_1

--- script.js
+++ script.js
@@ -244,7 +244,7 @@ function showOtherResultDetail(code) {
   otherResultDetail.classList.remove("hidden");
 }
 
-const CURRENT_ROUND = "test_1";
+const CURRENT_ROUND = "presentation_1";
 
 let statisticsListenerStarted = false;
 const STATISTICS_PARTICIPATION_KEY =
