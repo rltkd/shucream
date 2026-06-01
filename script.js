0587c888b3f3589c126f0e51999d9744c0cfd3d9
Baek Gisang <rltkd0101@gmail.com>
2026-06-01 15:41:29 +0900

    찐찐찐찐 최종

--- script.js
+++ script.js
@@ -244,7 +244,7 @@ function showOtherResultDetail(code) {
   otherResultDetail.classList.remove("hidden");
 }
 
-const CURRENT_ROUND = "test_9";
+const CURRENT_ROUND = "real_10";
 
 let statisticsListenerStarted = false;
 const STATISTICS_PARTICIPATION_KEY =
