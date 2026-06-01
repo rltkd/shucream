66af71f2d3f55c567f72eb0d9bf5bf2a2b8173f6
Baek Gisang <rltkd0101@gmail.com>
2026-06-01 15:18:06 +0900

    real_7

--- script.js
+++ script.js
@@ -244,7 +244,7 @@ function showOtherResultDetail(code) {
   otherResultDetail.classList.remove("hidden");
 }
 
-const CURRENT_ROUND = "real_6";
+const CURRENT_ROUND = "real_7";
 
 let statisticsListenerStarted = false;
 const STATISTICS_PARTICIPATION_KEY =
